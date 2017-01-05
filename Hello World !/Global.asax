499e681ac415960e37235c7e7127862bcb2792da
Ahmad Dimas Abid Muttaqi <ahmaddimas0@gmail.com>
2017-01-05 09:28:32 +0700

    Hello World

---
+++ Hello World !/Global.asax
@@ -0,0 +1,1 @@
+﻿<%@ Application Codebehind="Global.asax.cs" Inherits="Hello_World__.MvcApplication" Language="C#" %>
---
+++ Hello World !/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(Hello_World__.Startup))]
+namespace Hello_World__
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
